c1d5ecc0b5221762c39235aa6da62846c4cb1535
wrf <wrf@wla070.(none)>
2015-03-26 14:07:25 +0100

    addition of R and python scripts

---
+++ fastqdumps2histo.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+#
+# fastqdumps2histo.py v1.1 last modified 2015-03-25
+# by WRF
+
+"""
+fastqdumps2histo.py v1.1 2015-03-25
+
+    first generate fastq.counts file from zipped reads with jellyfish count
+gzip -dc reads.fastq.gz | jellyfish count -m 25 -o fastq.counts -C -U 1000 -s 1000000000 /dev/fd/0
+
+  add option -t for multithreads
+  change -m for different kmer sizes
+  change -U for maximum count, 1000 is good if genome coverage is 100+
+    otherwise perhaps use a value around 500
+    using a larger kmer will lower coverage, so if k > 45, set max to 500
+  change -s for memory allocation (see jellyfish manual)
+
+    fastq.counts file can be several gigabytes
+
+    jellyfish dump output would normally be quite large, so it is piped
+    then use jellyfish dump command as stdin
+jellyfish dump fastq.counts | fastqdumps2histo.py - > histo.csv
+
+    otherwise use as input file
+fastqdumps2histo.py fastq.dumps > histo.csv
+
+    if using a different kmer (other than 25, the default)
+    change with -k to correspond to the -m of jellyfish
+    also change -u to correspond to the -U of jellyfish
+
+    histo.csv is a comma separated matrix by GC count and coverage
+    coverage is taken from the fasta header for the kmer, which appears as:
+>123
+ACTTGATCGTGATGCTAGTAGCTGT
+
+    GC counts are integer counts of all Gs plus Cs
+    so the max value is the kmer length
+
+this file can be imported directly into R for generating lava lamp plots
+"""
+
+import sys
+import argparse
+import time
+
+def get_freq(line):
+	return int(line[1:].rstrip())
+
+def get_gc(kmer):
+	return kmer.count("G")+kmer.count("C")
+
+def main(argv, wayout):
+	if not len(argv):
+		argv.append("-h")
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=__doc__)
+	parser.add_argument('input_file', type = argparse.FileType('rU'), default = '-', help="fasta format file")
+	parser.add_argument('-k', '--kmer', type=int, metavar='N', default=25, help="kmer length [25]")
+	parser.add_argument('-s', '--separator', default=",", help="use alternate separator [,]")
+	parser.add_argument('-u', '--upper', type=int, metavar='N', default=1000, help="upper limit for histogram [1000]")
+	args = parser.parse_args(argv)
+
+	# generate kmer x maximum matrix
+	m = [[0 for x in range(args.maximum+1)] for y in range(args.kmer+1)]
+	kmercount = 0
+
+	print >> sys.stderr, "Reading fastq dumps", time.asctime()
+	for line in args.input_file:
+		# assumes jellyfish dump is in fasta format of kmers where header is count
+		# for header, take the count
+		if line[0]==">":
+			kmercount += 1
+			freq = get_freq(line)
+		# if not a header, then it is sequence
+		else:
+			seq = line.rstrip()
+			# calculate gc content for output
+			gc = get_gc(seq)
+			m[gc][freq] += 1
+
+	print >> sys.stderr, "Writing counts", time.asctime()
+	for i in m:
+		print >> wayout, args.separator.join([str(j) for j in i])
+
+	print >> sys.stderr, "Counted %d kmers" % (kmercount), time.asctime()
+
+if __name__ == "__main__":
+	main(sys.argv[1:],sys.stdout)
---
+++ jellyfish_gc_coverage_blob_plot_v2.R
@@ -0,0 +1,68 @@
+# plot jellyfish kmer coverage vs gc content in lava lamp form
+# v2 last modified 2015-03-26
+
+# generate csv matrix of outputs using fastqdumps2histo.py
+
+### CHANGE THIS FILE TO THE CSV OUTPUT
+f1 = "~/genomes/hydra_vulgaris/hydra_vulgaris_SRR1032106.k31.gc_vs_cov.csv"
+
+### CHANGE THIS VALUE BASED ON JELLYFISH -U OPTION
+# to subset or make the matrix smaller, change ymax
+ymax = 1000
+zmax = 1000000
+
+# read in data and create log of matrix for color coding
+# ymax here is also used to adjust the heatmap size
+d1 = read.table(f1, header=FALSE, sep=",", colClasses='integer')[,1:ymax]
+
+# at least one value is needed to make the matrix, in this case columns
+# this alternatively could be nrow of kmer
+m1 = matrix(unlist(d1), ncol=ymax, byrow=FALSE)
+xmax = dim(m1)[1]
+
+# instead calculate kmer as being number of rows - 1
+kmer = xmax - 1
+
+# this is needed to correct formatting of the GC percentage
+roundedxdim = 5 * round(xmax/5)
+gcpositions = pretty(c(0,roundedxdim))
+
+# since GC is just a count, not a percentage, convert it to rounded percentage
+gclabels = round(gcpositions / kmer * 100)
+
+# must calculate sums before normalizing
+d2 = colSums(m1)
+
+# chops all values greater than 1M to 1M
+m1[m1>zmax] = zmax
+# log all values to get better color resolution
+m2 = log(m1, base=2)
+
+# use this for log base 2
+# first function generates color scale from white to purple, then purple to red
+colorset = c(colorRampPalette(c("#FFFFFF","#FF00FF"),alpha=0.9)(100), rainbow(900, s=0.95, e=0.93, v=0.75, alpha=0.9) )
+
+# for coverage heatmap
+### UNCOMMENT AND RENAME FOR PDF OUTPUT
+#pdf(file="~/genomes/hydra_vulgaris/hydra_vulgaris_k31_vs_gc_with_line.pdf", width=10, heigh=8)
+
+par(mar=c(4,4,4,4))
+# this becomes the main chart label
+mainlab = paste(kmer,"mer coverage vs GC",sep="")
+# image() actually generates the heatmap
+image(x=1:ncol(m2), y=1:nrow(m2), z=t(m2), col=colorset, xlab="Coverage", ylab="GC%", axes=FALSE, main=mainlab )
+axis(1, at=pretty(c(0,ymax)), labels=pretty(c(0,ymax)) )
+# because 0 is a position, all other positions have to be corrected
+axis(2, at=gcpositions+1, labels=gclabels )
+
+### GENERATE LINE OVERLAY
+### COMMENT OUT PAR THROUGH PLOT TO REMOVE THIS
+par(new=TRUE, mar=c(4,2,2,2))
+# params for line generation
+xmax2 = length(d2)
+ymax2 = max(d2)
+yscale = ceiling(log(ymax2,base=10))
+plot(d2, type='l', xlab="Coverage", ylab="Unique kmers", xlim=c(0,xmax2), ylim=c(10^2,10^yscale), log='y', frame.plot=FALSE, axes=FALSE)
+
+### MUST UNCOMMENT FOR PDF OUTPUT
+#dev.off()
