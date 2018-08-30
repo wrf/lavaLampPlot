55ab8026de20057317db950e863cf99b770f1a21
wfrancis <wfrancis@biology.sdu.dk>
2018-08-30 13:02:52 +0200

    convert to 6 column with header

--- contig_gc_coverage.R
+++ contig_gc_coverage.R
@@ -1,6 +1,6 @@
-# contig gc coverage  last modified 2018-05-14
+# contig gc coverage  last modified 2018-08-29
 # output should be tab-delimited as as:
-# contigname  length  coverage  GC
+# contigname  contignumber  length  coverage  GC  gaps
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -8,13 +8,13 @@ inputfile = args[1]
 #inputfile = "~/genomes/tethya_wilhelma-genome/twilhelma_v2_dovetail/twilhelma_2014_dna_vs_dovetail_coverage.tab"
 outputfile = gsub("([\\w/]+)\\....$","\\1.pdf",inputfile,perl=TRUE)
 
-coveragedata = read.table(inputfile, header=FALSE, sep='\t')
+coveragedata = read.table(inputfile, header=TRUE, sep='\t')
 
 names = coveragedata[,1]
 
 covmax = 1000
 
-contiglengths = coveragedata[,2]
+contiglengths = coveragedata[["length"]]
 magnituderange = range(log(contiglengths,base=10))
 # size of each point
 pchsize = log(contiglengths,base=10)-magnituderange[1]
@@ -25,7 +25,7 @@ legendpch = legendsizes - magnituderange[1]
 
 totalsize = sum(contiglengths)
 
-gc = coveragedata[,4]
+gc = coveragedata[["GC"]]
 highgc = gc > 50
 sum(contiglengths[highgc])
 sum(contiglengths[!highgc])
@@ -42,10 +42,10 @@ pointcolor[massivecontigs] = "#d51ea477"
 # generate figure
 pdf(file=outputfile, width=8, height=7)
 par(mar=c(4.5,4.5,3,1))
-plot(coveragedata[,3], coveragedata[,4], type='p', xlim=c(0,covmax), ylim=c(20,80), xlab="Mean coverage of mapped reads", ylab="GC%", pch=16, frame.plot=FALSE, col=pointcolor, cex.axis=1.5, cex=pchsize, main=inputfile, cex.lab=1.4)
+plot(coveragedata[["coverage"]], coveragedata[["GC"]], type='p', xlim=c(0,covmax), ylim=c(20,80), xlab="Mean coverage of mapped reads", ylab="GC%", pch=16, frame.plot=FALSE, col=pointcolor, cex.axis=1.5, cex=pchsize, main=inputfile, cex.lab=1.4)
 legend(750,80, legend=legendlabels, pch=16, col=c("#39bc6799","#386edc99","#d51ea499"), pt.cex=legendpch, cex=1.1, title="Contig size (bp)")
-text(1000,23,paste(round(totalsize/1000000,digits=1),"Mb"), cex=1.2, pos=2)
-text(1000,20,paste(length(names),"total contigs"), cex=1.2, pos=2)
+text(covmax,23,paste(round(totalsize/1000000,digits=1),"Mb"), cex=1.2, pos=2)
+text(covmax,20,paste(length(names),"total contigs"), cex=1.2, pos=2)
 dev.off()
 
 
--- hits_to_coverage.py
+++ hits_to_coverage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-'''hits_to_coverage.py  last modified 2018-05-17
+'''hits_to_coverage.py  last modified 2018-08-30
 
     combine number of reads mapped to each contig with
     length and GC content, to make a tabular output as:
-seqID  length  coverage  GC%
+seqID  seqnumber  length  coverage  GC%  gaps
 
 hits_to_coverage.py -f contigs.fasta -b hits_from_bam.txt > coverage.tab
 
@@ -25,10 +25,10 @@ def main(argv, wayout):
 	parser.add_argument('-b', '--hits-from-bam', help="hits per contig, counted from the .bam file")
 	parser.add_argument('-f', '--fasta', help="fasta files of contigs or scaffolds")
 	parser.add_argument('-l', '--read-length', type=float, metavar='N', default=100.0, help="read length (must be constant)")
-	parser.add_argument('-A', '--above', type=int, metavar='N', default=0, help="retrieve kmers with a count greater than N [1]")
-	parser.add_argument('-B', '--below', type=int, metavar='N', default=10000, help="retrieve kmers with a count less than N [10000]")
-	parser.add_argument('-S', '--strong', type=float, metavar='0.F', default=0.0, help="retrieve kmers with GC percent greater than N [0.0]")
-	parser.add_argument('-W', '--weak', type=float, metavar='0.F', default=100.0, help="retrieve kmers with GC percent less than N [100.0]")
+	parser.add_argument('-A', '--above', type=int, metavar='N', default=0, help="keep contigs with a count greater than N [0]")
+	parser.add_argument('-B', '--below', type=int, metavar='N', default=10000, help="keep contigs with a count less than N [10000]")
+	parser.add_argument('-S', '--strong', type=float, metavar='0.F', default=0.0, help="keep contigs with GC percent greater than N [0.0]")
+	parser.add_argument('-W', '--weak', type=float, metavar='0.F', default=100.0, help="keep contigs with GC percent less than N [100.0]")
 	args = parser.parse_args(argv)
 
 	hitdict = {}
@@ -40,15 +40,17 @@ def main(argv, wayout):
 	seqcounter = 0
 	keptcounter = 0
 	print >> sys.stderr, "# Reading contigs from {}".format(args.fasta)
+	print >> sys.stdout, "scaffold\tnumber\tlength\tcoverage\tGC\tgaps"
 	for seqrec in SeqIO.parse(args.fasta,"fasta"):
 		seqcounter += 1
 		seqlength = len(seqrec.seq)
-		gc = (seqrec.seq.count("G")+seqrec.seq.count("C")) * 100.0 / ( len(seqrec.seq) - seqrec.seq.count("N") )
-		coverage = int(args.read_length * hitdict[seqrec.id] / seqlength)
-		if coverage < args.above or coverage > args.below or gc < args.strong or gc > args.weak:
+		gaps = (seqrec.seq.count("N")+seqrec.seq.count("n"))
+		gc = (seqrec.seq.count("G")+seqrec.seq.count("C")) * 100.0 / ( len(seqrec.seq) - gaps )
+		coverage = int(args.read_length * hitdict.get(seqrec.id,0) / seqlength)
+		if coverage <= args.above or coverage > args.below or gc < args.strong or gc > args.weak:
 			continue
 		keptcounter += 1
-		print >> sys.stdout, "{}\t{}\t{}\t{}".format( seqrec.id, seqlength, coverage, gc )
+		print >> sys.stdout, "{}\t{}\t{}\t{}\t{}\t{}".format( seqrec.id, seqcounter, seqlength, coverage, gc, gaps )
 	print >> sys.stderr, "# Counted {} sequences, kept {}".format( seqcounter, keptcounter )
 
 if __name__ == "__main__":
